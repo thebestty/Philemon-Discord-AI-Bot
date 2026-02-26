02b4efba043e00a0cf73303bebc616408f9fe49a
thebestty <123135589+thebestty@users.noreply.github.com>
2026-02-25 16:48:41 -0800

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 
 load_dotenv
-token = os.getenv("DISCORD_TOKEN")
+token = os.getenv("MTQ3Mzg0NzM4OTk3MzUxNjQxMQ.G7tBGy.ykvP1p3Amm8MC--ixEAgg-w5wkNd_xZZxeO-Tc")
 handler = logging.FileHandler(filename= 'discord.log', encoding= 'utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
